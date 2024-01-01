bb7e2877d8086d86c2fb7895aa947523a6b5a586
absolutekelvin273 <kelvinkakuru2@gmail.com>
2024-01-01 07:42:45 -0800

    Added a  simple array structure day 1

---
+++ array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//Declaration and Initialiazation of interger array
+int main() {:
+	int myArray[5];
+	myArray[0]=10;
+	myArray[1]=25;
+	myArray[2]=30;
+	myArray[3]=40;
+	myArray[4]=50;
+//Accessing the elements in the arrays
+printf("The element at position 1 is:  %d \n",myArray[1]);
+return 0
+}
